a1d17b04a29417319a37b6cfc770321a9d24a93e
Rye29 <167343946+Rye29@users.noreply.github.com>
2025-11-19 14:36:30 -0800

    created input manager
    -input manager has move_keys and player_mouse vectors that read WASD and the mouse respectiely
    -can create actions to poll by creating a new action in the list, assigning it a name and a keybinding
    -manager contains a public function GetAction(string ActionName), it searches and returns a reference to the action you searched for
    
    WARNING: do not modify any of the bools present in the inputaction struct, or it will break the script

---
+++ Assets/scripts/Input_Manager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct InputActionKey
+{
+    public string actionName;
+    public KeyCode code;
+    public bool held;
+    public bool down;
+    public bool released;
+    public InputActionKey(string Name, KeyCode keyCode)
+    {
+        this.actionName = Name;
+        this.code = keyCode;
+        this.held = false;
+        this.down = false;
+        this.released = false;
+    }
+}
+
+public class Input_Manager : MonoBehaviour
+{
+    public Vector2 move_keys = Vector2.zero;
+    public Vector2 player_mouse = Vector2.zero;
+    [SerializeField] public List<InputActionKey> actions = new List<InputActionKey>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move_keys.x = Input.GetAxisRaw("Horizontal");
+        move_keys.y = Input.GetAxisRaw("Vertical");
+
+        player_mouse.x = Input.GetAxisRaw("Mouse X");
+        player_mouse.y = Input.GetAxisRaw("Mouse Y");
+
+        for (int i = 0; i < actions.Count; i++)
+        {
+            InputActionKey iter = actions[i];
+            iter.held = Input.GetKey(iter.actionName);
+            iter.down = Input.GetKeyDown(iter.actionName);
+            iter.released = Input.GetKeyUp(iter.actionName);
+            actions[i] = iter;
+        }
+    }
+    
+    public InputActionKey GetAction(string name)
+    {
+        return actions.Find(button => button.actionName == name);
+    }
+}
